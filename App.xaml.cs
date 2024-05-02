34f06db7b5a5384ae3d2434498d463fb35184e1f
David Maw <david@maw.name>
2024-05-01 20:45:43 -0700

    Set App Size and Position

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new AppShell();
         }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+            // Set the App window to a sensible (phone like) size during initialization
+            if (DeviceInfo.Idiom == DeviceIdiom.Desktop)
+            {
+                window.Height = 400;
+                window.Width = 600;
+                window.X = 100;
+                window.Y = 100;
+            }
+            return window;
+        }
     }
 }
